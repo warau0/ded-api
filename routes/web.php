76e030516e023ecd3eac78034951c7d49fe06818
warau0 <larswalwes@gmail.com>
2019-05-21 23:44:27 +0200

    Mock route for notifications

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,6 @@ app('router')->post('login', 'AuthController@login');
 app('router')->get('key', function() { return str_random(32); });
 
 app('router')->group(['middleware' => 'auth'], function() {
-  //
+  // TODO
+  app('router')->get('notifications', function() { return ["notifications" => []]; });
 });
